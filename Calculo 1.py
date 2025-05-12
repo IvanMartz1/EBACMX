dd176017128b21b65173afc100dcec5bdb216981
Ivan Martinez <ivan.martz23@outlook.com>
2025-05-12 17:27:45 -0600

    Se agrega un nuevo archivo al git

---
+++ Calculo 1.py
@@ -0,0 +1,13 @@
+# Ejemplo de calculo para Branch
+# Se realizaran duversos cambios sin embargo la primera parte se generar a consideracion del autor
+a = 10
+b = 30
+c = 30 * 10 * 5
+
+print ('resultado: ', c)
+
+# Cambiando el factor 102 segun requerimientos del negocio
+# Cambiando el factor 110 segun junta de control
+
+d = c *110
+print ('resultado total: ', d)
